2a46e651a6485dc677a17fee7f4e042b9e269e24
Improveiz <aznsuccess@gmail.com>
2022-10-12 09:54:18 -0700

    pass gen

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import random
+import string
 
+length = int(input('Enter length of password: '))
+
+letters = string.ascii_letters
+numb = string.digits
+punc = string.punctuation
+
+all = letters+numb+punc
+
+temp = random.sample(all,length)
+
+password = ''.join(temp)
+
+print(password)
